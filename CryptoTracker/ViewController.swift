5d614ddd44e2a109cecfc670a90da94a7fb63a77
Haz-15 <Ab2388@njit.edu>
2021-04-27 00:30:54 -0400

    The entire project

--- CryptoTracker/ViewController.swift
+++ CryptoTracker/ViewController.swift
@@ -9,11 +9,88 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+  
+        
+        if let symbol = textField.text { 
+            getData(symbol: symbol)
+        }
+    
+    
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
-}
+    var url = "https://min-api.cryptocompare.com/data/price?tsyms=USD&fsym="
+    
+    func getData(symbol : String){
+        
+        url = "\(url)&fsym=\(symbol)"
+        
+        guard let url = URL(string: url) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) {
+            (data, _, error) in
+            
+            guard let data = data, error == nil else { return }
+        
+        
+        print("Data Recieved!")
+        
+        
+        do {
+            
+            let Result = try JSONDecoder().decode(APIResponse.self, from: data)
+                            print(Result.USD)
+            
+            DispatchQueue.main.async {
+                
+                self.outputLabel.text = "\(Result.USD)"
+                
+            }
+            
+        }
+       
+        
+        catch {
+            print(error.localizedDescription)
+        
+        }
+        
+        
+    }
+    
+    task.resume();
+    
+    
 
+    }
+
+    struct APIResponse : Codable {
+        let main : [Result]
+        let USD: Float
+ 
+    }
+
+    struct Result : Codable {
+     
+        let temp : Float
+        let feels_like : Float
+    
+    }
+
+}
